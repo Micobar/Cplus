c2b0c78fa9a5292eafdfa00759a1b092ec15b0ec
Micobar <149310653+Micobar@users.noreply.github.com>
2024-03-16 13:27:08 +0000

    Add files via upload

---
+++ assignment_operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int num1 {10}; // this is called initialisation
+    int num2 {20};
+    
+    num1 = 100; // this is called assignment
+    
+    cout << "num1: " << num1 << endl;
+    cout << "num2: " << num2 << endl;
+    
+    
+    
+    return 0;
+}
